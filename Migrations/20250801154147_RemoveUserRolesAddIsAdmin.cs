dacadac329c3b763ad76d24e1a176243d220bfad
Mason Turvey <mst@silverinc.org>
2025-08-01 10:42:31 -0500

    Add EF migration: RemoveUserRolesAddIsAdmin
    - Remove legacy Roles property (text[] array) from Users table
    - Add IsAdmin boolean column with default value false
    - Update database schema to support new authorization system
    - Migration applied successfully to database

---
+++ Migrations/20250801154147_RemoveUserRolesAddIsAdmin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sonic.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserRolesAddIsAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Roles",
+                table: "Users");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Users");
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "Roles",
+                table: "Users",
+                type: "text[]",
+                nullable: false);
+        }
+    }
+}
